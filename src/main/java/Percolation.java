1bf2bea64c0fc94c88dbc98ebf430064acff98fc
Giovanny <giovanny.sayas@akendos.com>
2018-08-18 18:05:06 +0100

    open site

--- src/main/java/Percolation.java
+++ src/main/java/Percolation.java
@@ -1,2 +1,19 @@
+import edu.princeton.cs.algs4.StdRandom;
+import edu.princeton.cs.algs4.StdStats;
+import edu.princeton.cs.algs4.WeightedQuickUnionUF;
+
 public class Percolation {
+
+    private WeightedQuickUnionUF wqu;
+
+    public Percolation() {
+        wqu = new WeightedQuickUnionUF(10);
+    }
+
+    public boolean isOpen(int i, int i1) {
+        return false;
+    }
+
+    public void open(int i, int i1) {
+    }
 }
--- src/test/java/PercolationTest.java
+++ src/test/java/PercolationTest.java
@@ -5,8 +5,17 @@ import static org.junit.Assert.assertEquals;
 
 public class PercolationTest {
 
+    private Percolation percolation;
+
+    @Before
+    public void setUp() throws Exception {
+        percolation = new Percolation();
+    }
+
     @Test
-    public void name() {
-        
+    public void testOpenSite() {
+        assertEquals(false, percolation.isOpen(1,1) );
+        percolation.open(1,1);
+        assertEquals(true, percolation.isOpen(1,1) );
     }
 }
